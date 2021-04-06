c18a3504d132dad25ac14ae479826f70c476c14a
Andrew Baker <andrew_j_baker2@hotmail.com>
2021-04-06 12:12:40 +0100

    Calculate and display number of correct answers

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { RouteComponentProps } from "@reach/router";
 import Button from "../../components/Button/Button";
 import "./Results.scss";
@@ -6,9 +7,25 @@ import "./Results.scss";
 interface ResultsIProps extends RouteComponentProps {}
 
 function Results(props: ResultsIProps) {
+  const numberOfQuestions = useSelector(
+    (state: any) => state.trivia.questions.length
+  );
+
+  const numberOfCorrectAnswers = useSelector(
+    (state: any) =>
+      state.trivia.questions.filter(
+        (question: any, index: number) =>
+          question.correct_answer === state.trivia.answers[index]
+      ).length
+  );
+
   return (
     <div className="results">
       <h1>Thank You!</h1>
+      <p>
+        You answered {numberOfCorrectAnswers} out of {numberOfQuestions}{" "}
+        questions correctly.
+      </p>
       <div>
         <Button onClick={() => props.navigate && props.navigate("/")}>
           Start New Quiz
